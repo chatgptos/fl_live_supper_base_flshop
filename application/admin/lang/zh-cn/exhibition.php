e6c6e731d75988fd47f7f7e8f3668e2d95364ef6
1276789849@qq.com <1276789849@qq.com>
2022-06-28 09:01:36 +0800

    business.service

--- application/admin/lang/zh-cn/exhibition.php
+++ application/admin/lang/zh-cn/exhibition.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
-    'Id'              => '展会id',
-    'Exhibition_name' => '展会名称',
-    'Name'            => '展会名称',
-    'Start_time'      => '展会开始时间',
-    'End_time'        => '展会结束时间',
-    'Status'          => '展会状态',
-    'Remark'          => '展会备注信息'
+    'Id'                    => '展会id',
+    'Exhibition_name'       => '展会名称',
+    'Name'                  => '展会名称',
+    'Business_services_img' => '商旅服务',
+    'Traffic_img'           => '交通路线图',
+    'Service'               => '场馆服务',
+    'Start_time'            => '展会开始时间',
+    'End_time'              => '展会结束时间',
+    'Status'                => '展会状态',
+    'Remark'                => '展会备注信息'
 ];
--- application/api/controller/flbooth/BoothHall.php
+++ application/api/controller/flbooth/BoothHall.php
@@ -15,7 +15,7 @@ class BoothHall extends Api
     //如果接口已经设置无需登录,那也就无需鉴权了
     //
     // 无需登录的接口,*表示全部
-    protected $noNeedLogin = ['getHallById','getHallActivityGuideById'];
+    protected $noNeedLogin = ['getHallById','getHallActivityGuideById','getHallServiceById'];
     // 无需鉴权的接口,*表示全部
     protected $noNeedRight = ['test2'];
 
@@ -58,7 +58,7 @@ class BoothHall extends Api
      * @ApiReturn   ({"code":1,"msg":"","time":"1655878085","data":{"id":1,"exhibition_id":5,"hall_name":"2012中国国际渔业博览会海外展区","booths_num":0,"hall_map":".\/home\/img\/16558173804170.jpg","hall_addr":"2012中国国际渔业博览会海外展区","map_height":578,"map_width":845,"hall_namein":"A"}})
 
      */
-    public function getHallActivityGuideById()
+    public function getHallAreaGuideById()
     {
         $exhibition_id = $this->request->post("id",0);//展会id  中食展,云选展
 
@@ -71,6 +71,43 @@ class BoothHall extends Api
             ->field('*')
             ->order('created desc')
             ->select();
+
+
+        $where['is_recommend'] = 1;
+        //获取推荐活动
+        $recommend_exhibitor = \app\admin\model\Boothinfo::where($where)->select();
+        $data['recommend_exhibitor']=$recommend_exhibitor;
+        $this->success('', $data);
+    }
+
+
+
+    /**
+     * 场馆服务
+     *
+     * @ApiTitle    (场馆服务)
+     * @ApiSummary  (场馆服务)
+     * @ApiMethod   (POST)
+     * @ApiParams   (name="id", type="integer", required=true, description="展会id 中食展1")
+     * @ApiReturnParams   (name="code", type="integer", required=true, sample="0")
+     * @ApiReturnParams   (name="msg", type="string", required=true, sample="返回成功")
+     * @ApiReturnParams   (name="data", type="object", sample="{'user_id':'int','user_name':'string','profile':{'email':'string','age':'integer'}}", description="扩展数据返回")
+     * @ApiReturn   ({"code":1,"msg":"","time":"1655878085","data":{"id":1,"exhibition_id":5,"hall_name":"2012中国国际渔业博览会海外展区","booths_num":0,"hall_map":".\/home\/img\/16558173804170.jpg","hall_addr":"2012中国国际渔业博览会海外展区","map_height":578,"map_width":845,"hall_namein":"A"}})
+
+     */
+    public function getHallServiceById()
+    {
+        $exhibition_id = $this->request->post("id",0);//展会id  中食展1,云选展2
+
+        $where['id'] = $exhibition_id;
+        $where=array_filter($where);
+
+
+        $data['hall_list'] = model('app\admin\model\Exhibition')
+            ->where($where)
+            ->field('*')
+            ->order('created desc')
+            ->find();
 
 
         $where['is_recommend'] = 1;
